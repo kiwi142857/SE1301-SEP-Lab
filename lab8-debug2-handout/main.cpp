aa394d637b4eab917f9d4a497291877f83d66c71
kiwi142857 <779490624@qq.com>
2023-11-13 11:26:36 +0800

    modify lab8/main.cpp to do fully test
    enlargening test range as well as test cases.
    
    Signed-off-by: kiwi142857 <779490624@qq.com>

--- lab8-debug2-handout/main.cpp
+++ lab8-debug2-handout/main.cpp
@@ -1,13 +1,43 @@
 #include "tree.h"
-
 #include <cassert>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+void test_tree() {
+  std::vector<int> numbers;
+  for (int i = 0; i < 100; ++i) {
+    numbers.push_back(rand() % 10000 + 1);
+  }
+
+  std::shuffle(numbers.begin(), numbers.end(), std::mt19937{std::random_device{}()});
+
+  tree_t tree(100);
+  for (const auto& number : numbers) {
+    tree.set(number, number);
+    assert(tree.get(number) == number);
+  }
+
+  std::shuffle(numbers.begin(), numbers.end(), std::mt19937{std::random_device{}()});
+
+  for (const auto& number : numbers) {
+    assert(tree.get(number) == number);
+  }
+
+  std::shuffle(numbers.begin(), numbers.end(), std::mt19937{std::random_device{}()});
+
+  // remove half of the numbers
+  for (int i = 0; i < 50; ++i) {
+    tree.remove(numbers[i]);
+  }
+
+  // test the remaining numbers
+  for (int i = 50; i < 100; ++i) {
+    assert(tree.get(numbers[i]) == numbers[i]);
+  }
+}
 
 int main() {
-  tree_t tree(3);
-  tree.set(1, 42);
-  auto v = tree.get(1);
-  assert(v == 42);
-  tree.remove(1);
-
+  test_tree();
   return 0;
 }
